532415277c2e867abef8e102f5ece15946cd1af5
Nick Smith <nick.smith.asu@gmail.com>
2024-01-16 17:46:03 -0700

    add database and create tables

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employees_db;
+
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY(department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employees(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    FOREIGN KEY(role_id) REFERENCES role(id),
+    manager_id INT,
+    FOREIGN KEY(manager_id) REFERENCES employees(id)
+);
