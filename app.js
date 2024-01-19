8aa84092f898bab72108417b1dc261afac0fcad5
Nick Smith <nick.smith.asu@gmail.com>
2024-01-18 17:56:33 -0700

    add queries to app.js

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-const mysql2 = require('mysql2');
+const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: 'root',
+        database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+);
+
 
 // Prompt for user action
 inquirer
@@ -23,13 +37,37 @@ inquirer
         // Call the appropriate function based on the user's choice
         switch (answer.action) {
             case 'View all departments':
-                // Call a function to view all departments
+                // Query database
+                db.query(`SELECT * FROM department`, function (err, results) {
+                    console.table(results);
+                });
                 break;
             case 'View all roles':
-                // Call a function to view all roles
+                // Query database
+                db.query(`SELECT role.id, role.title,department.name AS department, role.salary
+                FROM role
+                INNER JOIN department on role.department_id = department.id
+                ORDER BY role.id`, function (err, results) {
+                    console.table(results);
+                });
                 break;
             case 'View all employees':
-                // Call a function to view all employees
+                // Query database
+                db.query(`SELECT
+                e.id, 
+                e.first_name,
+                e.last_name, 
+                r.title,
+                d.name AS department, 
+                r.salary, 
+                CONCAT(m.first_name, ' ', m.last_name) AS manager
+                FROM employees e
+                INNER JOIN role r ON e.role_id = r.id
+                INNER JOIN department d ON r.department_id = d.id
+                LEFT JOIN employees m ON e.manager_id = m.id
+                ORDER BY e.id`, function (err, results) {
+                    console.table(results);
+                });
                 break;
             case 'Add a department':
                 // Call a function to add a department
